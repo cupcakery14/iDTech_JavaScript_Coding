def3d2ccdf6c258f1df7cf781433e11311d192aa
Pallavi <asp.inamdar@gmail.com>
2022-07-08 14:20:31 -0400

    hihihi

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,34 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let protagonist = ' Annie.';
+let antagonist = 'The Evil Sorceress';
+let kingdom_name = 'Darkonn';
+let hobby = 'collect treasures';
+
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let protagonist_age = 17;
+let century = 170;
+let year = 17000;
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let item_powers = ['gold necklace.', 'invisibility', '3500 precious treasures.', 'magic wand!', 'teleportation'];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let favorite_food_pizza = true;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log("Hey there! My name is" + protagonist);
+console.log("I am " + protagonist_age + " years old.");
+console.log("I like to " + hobby + " from around the world.")
+console.log("The year is " + year + "." + " We are in the " + century + "th century.");
+console.log("Welcome to " + kingdom_name + ".");
+console.log(antagonist + " rules over " + kingdom_name + ". " + antagonist + " is very bad and is the villian of my world.");
+console.log(antagonist + " is trying to steal my powers of " + item_powers[1] + " and " + item_powers[4] + ".");
+console.log(antagonist + " is jealous of my " + item_powers[2] + " She wants my " + item_powers[0]);
+console.log("I won't let her have my powers or treasures. Join me in my mission to defeat " + antagonist + " with my " + item_powers[3]);
+console.log("Also, you may be wondering if my favorite food is pizza. Well, the answer is: " + favorite_food_pizza + "!");
